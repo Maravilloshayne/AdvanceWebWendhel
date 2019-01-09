d8d1f7b9f292747eee95d70b70609a4c6403c09f
Shayne Maravillo <yashnedobasa@gmail.com>
2019-01-09 14:12:46 +0800

    AdvanceWebWendhelAton

---
+++ AdvanceWebWendhel.Web/ViewModels/Users/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using AdvanceWebWendhel.Web.Infrastructures.Data.Helpers;
+using AdvanceWebWendhel.Web.Infrastructures.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvanceWebWendhel.Web.ViewModels.Users
+{
+    public class IndexViewModel
+    {
+        public Page<User> Users { get; set; }
+    }
+}
